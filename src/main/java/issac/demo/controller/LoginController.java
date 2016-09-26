b4ddc66b351755953b1833fbfb8cd31d9224a571
issac <humaoyongyx@163.com>
2016-09-26 10:40:28 +0800

    根据浏览器设置，增加对国际化i18n的支持

--- src/main/java/issac/demo/controller/LoginController.java
+++ src/main/java/issac/demo/controller/LoginController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
@@ -11,6 +12,11 @@ public class LoginController {
 	@RequestMapping("/main")
 	public String mainPage(HttpServletRequest request) {
 		return "main";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String loginPage(HttpServletRequest request) {
+		return "login";
 	}
 
 }
