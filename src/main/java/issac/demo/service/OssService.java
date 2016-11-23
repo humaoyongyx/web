0a0c9841f035809bd0cc387c8ed65016accf28fe
issac <humaoyongyx@163.com>
2016-11-23 10:38:34 +0800

    增强oss工具类

--- src/main/java/issac/demo/service/OssService.java
+++ src/main/java/issac/demo/service/OssService.java
@@ -4,12 +4,17 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.apache.log4j.Logger;
+
 import com.aliyun.oss.ClientException;
 import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.OSSException;
+import com.aliyun.oss.model.OSSObject;
 import com.aliyun.oss.model.ObjectMetadata;
 
 public class OssService {
+	
+	private Logger logger=Logger.getLogger(OssService.class);
 	private String endpoint;
 	private String accessKeyId;
 	private String accessKeySecret;
@@ -17,22 +22,22 @@ public class OssService {
 	private String accessUrl;
 
 	public String uploadFile(String key, File file) throws IOException {
-		OSSClient client = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+		OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
 		try {
-			client.putObject(bucketName, key, file);
-
+			logger.info("upload file "+key+" to OSS!");
+			ossClient.putObject(bucketName, key, file);
 		} catch (OSSException oe) {
 			oe.printStackTrace();
 		} catch (ClientException ce) {
 			ce.printStackTrace();
 		} finally {
-			client.shutdown();
+			ossClient.shutdown();
 		}
 		return accessUrl + key;
 	}
 
 	public String uploadFile(String key, InputStream file, String contentType) {
-		OSSClient client = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+		OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
 		ObjectMetadata objectMetadata = new ObjectMetadata();
 		try {
 			objectMetadata.setContentLength(file.available());
@@ -41,51 +46,117 @@ public class OssService {
 		}
 		objectMetadata.setCacheControl("no-cache");
 		objectMetadata.setHeader("Pragma", "no-cache");
-		//	objectMetadata.setContentType(contentType);
+		if (contentType == null || contentType.trim().equals("")) {
+			int lastIndexOf = key.lastIndexOf(".");
+			String fileExtensionName = "";
+			if (lastIndexOf > -1) {
+				fileExtensionName = key.substring(lastIndexOf + 1);
+			}
+			contentType = getContentType(fileExtensionName);
+		}
+		objectMetadata.setContentType(contentType);
 		try {
-			client.putObject(bucketName, key, file, objectMetadata);
+			logger.info("upload file "+key+" to OSS!");
+			ossClient.putObject(bucketName, key, file, objectMetadata);
 
 		} catch (OSSException oe) {
 			oe.printStackTrace();
 		} catch (ClientException ce) {
 			ce.printStackTrace();
 		} finally {
-			client.shutdown();
+			ossClient.shutdown();
 		}
 		return accessUrl + key;
 	}
 
-	public static String contentType(String FileNameExtension) {
-		if (FileNameExtension.toLowerCase().equals("bmp")) {
+	public InputStream downloadOssFile(String key) {
+		OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+		try {
+			boolean exists = ossClient.doesObjectExist(bucketName, key);
+			if (exists) {
+				logger.info("download file "+key+" from OSS!");
+				OSSObject object = ossClient.getObject(bucketName, key);
+				logger.info("Content-Type: " + object.getObjectMetadata().getContentType());
+				return object.getObjectContent();
+			}
+		} catch (OSSException oe) {
+			oe.printStackTrace();
+		} catch (ClientException ce) {
+			ce.printStackTrace();
+		} finally {
+			ossClient.shutdown();
+		}
+
+		return null;
+
+	}
+	/**
+	 * 
+	 * @param key accessURL+key
+	 * @return
+	 */
+	public InputStream downloadFile(String url) {
+		return downloadOssFile(url.replace(accessUrl, ""));
+	}
+	public boolean deleteOssFile(String key) {
+		OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+		try {
+			boolean exists = ossClient.doesObjectExist(bucketName, key);
+			if (exists) {
+				logger.info("delete file "+key+" from OSS!");
+				ossClient.deleteObject(bucketName, key);
+				return true;
+			}
+		} catch (OSSException oe) {
+			oe.printStackTrace();
+		} catch (ClientException ce) {
+			ce.printStackTrace();
+		} finally {
+			ossClient.shutdown();
+		}
+		return true;
+
+	}
+	
+	/**
+	 * 
+	 * @param key accessURL+key
+	 * @return
+	 */
+	public boolean deleteFile(String url) {
+		return deleteOssFile(url.replace(accessUrl, ""));
+	}
+
+	private static String getContentType(String fileExtensionName) {
+		if (fileExtensionName.toLowerCase().equals("bmp")) {
 			return "image/bmp";
 		}
-		if (FileNameExtension.toLowerCase().equals("gif")) {
+		if (fileExtensionName.toLowerCase().equals("gif")) {
 			return "image/gif";
 		}
-		if (FileNameExtension.toLowerCase().equals("jpeg") || FileNameExtension.toLowerCase().equals("jpg") || FileNameExtension.toLowerCase().equals("png")) {
+		if (fileExtensionName.toLowerCase().equals("jpeg") || fileExtensionName.toLowerCase().equals("jpg") || fileExtensionName.toLowerCase().equals("png")) {
 			return "image/jpeg";
 		}
-		if (FileNameExtension.toLowerCase().equals("html")) {
+		if (fileExtensionName.toLowerCase().equals("html")) {
 			return "text/html";
 		}
-		if (FileNameExtension.toLowerCase().equals("txt")) {
+		if (fileExtensionName.toLowerCase().equals("txt")) {
 			return "text/plain";
 		}
-		if (FileNameExtension.toLowerCase().equals("vsd")) {
+		if (fileExtensionName.toLowerCase().equals("vsd")) {
 			return "application/vnd.visio";
 		}
-		if (FileNameExtension.toLowerCase().equals("pptx") || FileNameExtension.toLowerCase().equals("ppt")) {
+		if (fileExtensionName.toLowerCase().equals("pptx") || fileExtensionName.toLowerCase().equals("ppt")) {
 			return "application/vnd.ms-powerpoint";
 		}
-		if (FileNameExtension.toLowerCase().equals("docx") || FileNameExtension.toLowerCase().equals("doc")) {
+		if (fileExtensionName.toLowerCase().equals("docx") || fileExtensionName.toLowerCase().equals("doc")) {
 			return "application/msword";
 		}
-		if (FileNameExtension.toLowerCase().equals("xml")) {
+		if (fileExtensionName.toLowerCase().equals("xml")) {
 			return "text/xml";
 		}
 		return "application/octet-stream";
 	}
-
 
 	public String getEndpoint() {
 		return endpoint;
