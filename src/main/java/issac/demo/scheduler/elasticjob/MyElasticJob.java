cdbe8761f8d2ae44cca81144c01ad1deced8e7ca
Maoyong Hu <humaoyongyx@163.com>
2017-08-11 15:37:45 +0800

    简单的增加elasticjob测试

---
+++ src/main/java/issac/demo/scheduler/elasticjob/MyElasticJob.java
@@ -0,0 +1,24 @@
+package issac.demo.scheduler.elasticjob;
+
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
+
+public class MyElasticJob implements SimpleJob {
+    
+    @Override
+    public void execute(ShardingContext context) {
+    	System.out.println(context);
+        switch (context.getShardingItem()) {
+            case 0: 
+                // do something by sharding item 0
+                break;
+            case 1: 
+                // do something by sharding item 1
+                break;
+            case 2: 
+                // do something by sharding item 2
+                break;
+            // case n: ...
+        }
+    }
+}
