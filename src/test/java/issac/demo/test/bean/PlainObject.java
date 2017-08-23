ebd779e77bac4e0ec1c154e997030fd8f899accc
Maoyong Hu <humaoyongyx@163.com>
2017-08-23 17:41:53 +0800

    1

--- src/test/java/issac/demo/test/bean/PlainObject.java
+++ src/test/java/issac/demo/test/bean/PlainObject.java
@@ -4,7 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-
+/**
+ *   eclipse.ini / myeclipse.ini，在最后面插入以下两行并保存：
+        -Xbootclasspath/a:lombok.jar
+        -javaagent:lombok.jar
+ * @author admin
+ *
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
